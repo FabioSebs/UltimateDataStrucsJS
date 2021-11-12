d42bc67c54b9788a3789f808d31cfb728cca702b
FabioSebs <sebsphotosofficial@gmail.com>
2021-11-12 18:53:40 -0500

    selection sort

--- SelectionSort/selectionSort.js
+++ SelectionSort/selectionSort.js
@@ -1,16 +1,33 @@
 
-
 const selectionSort = (arr) => {
+    //looping through array
     for (let i = 0; i < arr.length - 1; i++) {
-        let min = i
+        
+	//minimum variable
+	let min = i
+
+	//nested for loop to compare two elements at a time
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[min]) {
+	    //comparing index i+1 to i 
+            if (arr[j] <= arr[min]) {
                 min = j
             }
-            [arr[i], arr[min]] = [arr[min], arr[i]]
+	    //swapping
+	    [arr[i], arr[min]] = [arr[min], arr[i]]
         }
     }
     return arr
 }
 
-console.log(selectionSort([1, 4, 2, 8, 345, 123, 43, 32]))
+const getData = (number) => {
+	let arr = []
+	for (let i = 0; i <= number; i++) {
+		arr.push(Math.round(Math.random() * number))
+	}
+	return arr
+}
+
+thirtyTwoArray = selectionSort(getData(32))
+oneTwentyEightArray = selectionSort(getData(128))
+console.log(selectionSort(thirtyTwoArray))
+console.log(selectionSort(oneTwentyEightArray))
